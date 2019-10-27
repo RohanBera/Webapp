4bb6749be412155a33f84ae80556b6513f6df56b
Rohan Bera <berarohan88@gmail.com>
2019-10-27 18:12:46 +0530

    resigtration back end works

--- signup.php
+++ signup.php
@@ -4,11 +4,11 @@
     $uname = "root";
     $pwd   = "password";
     $db    = "dbms_miniproj";
-    
-    $conn = new mysqli_connect($host, $uname, $pwd, $db);
+
+    $conn = mysqli_connect($host, $uname, $pwd, $db);
     if (mysqli_connect_error())
     {
-        die('Connection error ('. mysqli_connect_errno().')'. mysqli_connect_error());
+        die("Connection error (". mysqli_connect_errno().")". mysqli_connect_error());
     }
 
     // inserting values
@@ -17,17 +17,19 @@
     $fname = mysqli_real_escape_string($conn, $_POST['fname']);
     $lname = mysqli_real_escape_string($conn, $_POST['lname']);
     $email = mysqli_real_escape_string($conn, $_POST['mail']);
-    $plan  = mysqli_real_escape_string($conn, $_POST['plan']);    
+    $plan  = mysqli_real_escape_string($conn, $_POST['plan']);
     $passwd= mysqli_real_escape_string($conn, $_POST['pwd']);
     
-    $query = 'INSERT INTO register (FNAME, LNAME, EMAIL, PASSWORD, PLAN) VALUES ("'.$fname.'", "'.$lname.'", "'.$email.'", "'.md5($passwd).'", "'.$plan.'"';
+    $query = 'INSERT INTO register (FNAME, LNAME, EMAIL, PASSWORD, PLAN) VALUES ("'.$fname.'", "'.$lname.'", "'.$email.'", "'.md5($passwd).'", "'.$plan.'")';
 
-    if (mysqli_query($conn, $query))
+    if (!mysqli_query($conn, $query))
     {
-        echo ('registration succesful. Sign In <a href="signin.html">here</a>');
+        echo ("Email id already exists. Try another email id, or Sign In <a href=\"signin.html\">here</a>");
     }
-    else 
+    else
     {
-        echo ('Email id already exists. Try another email id, or Sign In <a href="signin.html">here</a>');
+        echo ("registration succesful. Sign In <a href=\"signin.html\">here</a>");
     }
+
+    mysqli_close($conn);
 ?>
