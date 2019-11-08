f8dc70dccefcb31e27d641d33d28c2537f90bf59
Rohan Bera <55288011+RohanBera@users.noreply.github.com>
2019-11-08 17:05:51 +0530

    Final header file. looks cute.

--- header.php
+++ header.php
@@ -1,44 +1,24 @@
+<?php session_start(); ?>
 <html>
     <head>
-        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js">  </script>
-
-        <script>
-            $(document).ready(function (){
-                $('li a').click(function(e){
-                    $('li.active').removeClass('active');
-
-                    var $child  = $(this);
-                    var $parent = $(this).parent();
-                    $parent.addClass('active');
-                    e.preventDefault();
-
-                    var temp = $child.attr("href");
-                    window.location.href = temp;
-                });
-            });        
-        </script> -->
-
         <style>
             ul {
                 list-style-type: none;
                 margin: 0;
                 padding: 0;
                 overflow: hidden;
-                background-color: #333;
+                background-color: rgb(44, 44, 44);
 
                 position: fixed;
                 top: 0;
                 width: 100%;
+                border-bottom: 1px solid black;
             }
 
             li {
                 float: left;
                 border-right: 1px solid white;
             }
-
-            /* li:last-child {
-                border-right: none;
-            } */
 
             li a {
                 display: block;
@@ -51,6 +31,8 @@
 
             li a:hover {
                 background-color: black;
+                align-self: baseline;
+                padding-bottom: 12px; 
                 box-shadow: 0 0 2px 2px orange;
             }
 
@@ -62,11 +44,34 @@
 
     <body>
         <ul>
-            <li><a href>Home</a></li>
-            <li><a href="series.php">Series</a></li>
-            <li><a>Contact</a></li>
-            <li style="float: right"><a href="signup_fe.php">Sign Up</a></li>
-            <li style="float: right; border-left: 1px solid white"><a href="signin_fe.php">Sign In</a></li>
+            <li id="home" ><a href="welcome.php">Home</a></li>
+            <li id="movies" ><a href="movies.php">Movies</a></li>
+            <li id="series" ><a href="series.php">Series</a></li>
+            <li id="Contact" ><a>Contact</a></li>
+            <?php
+                if (isset($_SESSION['active_user']))
+                {
+                    echo '<li id="user" style="float: right; border-left: 1px solid white"><a id="userprof" href="logout.php">Logout '.$_SESSION["active_user"].'</a></li>';
+                }
+                else
+                {
+                    echo '<li id="user" style="float: right;"><a id="userprof">Profile</a></li>';
+                    echo '<li id="signup" style="float: right;"><a href="signup_fe.php">Sign Up</a></li>';
+                    echo '<li id="signin" style="float: right; border-left: 1px solid white"><a href="signin_fe.php">Sign In</a></li>';
+                }
+            ?>    
+            
+            <!-- <?php 
+            if (isset($_SESSION['active_user']))
+            {
+                echo '<script>';
+                echo 'var signin = document.getElementById("signin");';
+                echo 'var signup = document.getElementById("signup");';
+                echo 'signin.style.display = "none";';
+                echo 'signup.style.display = "none";';
+                echo '</script>';
+            }
+            ?> -->
         </ul>
     </body>
 </html>
