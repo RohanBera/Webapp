98cfa3438af67a467a5d5a1b851066dcf5c7a9ef
Rohan Bera <55288011+RohanBera@users.noreply.github.com>
2019-11-09 12:53:40 +0530

    Admin block yaaaaayyyy!
    Its just 3 buttons. Does nothign so far.

---
+++ admin.php
@@ -0,0 +1,49 @@
+<html>
+    <head>
+        <style>
+            h1 {
+                margin-top: 0px;
+                margin-bottom: 10px;
+
+                font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+                color: white;
+            }
+
+            .adminbutton {
+                width: 20%;
+                height: 50px;
+                background-color: darkorange;
+
+                font-size: 110%;
+                font-weight: bold;
+                color: white;
+                
+                margin-top: 40px;
+                margin-bottom: 20px;
+                border: 0px;
+                border-radius: 10px;
+            }
+
+            a { 
+                color: white;
+            }
+
+            a:link {
+                text-decoration: none;
+            }
+        </style>
+
+    </head>
+
+    <body>
+        <h1>Hi Admin!</h1>
+
+        <button class="adminbutton"><a href="addadmin.php">Add a new Admin</a></button>
+        &nbsp;&nbsp;
+        <button class="adminbutton"><a href="addmovie.php">Add a new Movie</a></button>
+        &nbsp;&nbsp;
+        <button class="adminbutton"><a href="addseries1.php">Add a new Series</a></button>
+        &nbsp;&nbsp;
+
+    </body>
+</html>
